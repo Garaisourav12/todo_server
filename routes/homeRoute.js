3712dc9e0b6f3b578dfc207f4ee700bf8b979848
Garaisourav12 <garaisourav12@gmail.com>
2024-03-31 20:34:48 +0530

    home route

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 // Home route
 homeRouter.get('/', (req, res) => {
     // Read index.html file synchronously
-    const htmlContent = fs.readFileSync('index.html', 'utf8');
+    // const htmlContent = fs.readFileSync('index.html', 'utf8');
     
     // Send the HTML content as response
-    return res.send(htmlContent);
-    // return res.send('Welsome to The To-Do Server');
+    // return res.send(htmlContent);
+    return res.send('Welsome to The To-Do Server');
 });
 
 module.exports = homeRouter;
